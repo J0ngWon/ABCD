dc666b3e023ffce5d10e20bb00ac6c17fb3fb0fb
J0ngWon <129199902+J0ngWon@users.noreply.github.com>
2023-05-24 14:45:57 +0900

    test

---
+++ test.py
@@ -0,0 +1,14 @@
+import random
+N=int(input())  #카드수 3~10
+M=int(input())  #최대수 10~300
+card=[]
+
+for k in range(N):
+    card_number=int(random(1,M))
+    if card_number in card :
+        N-=1
+        continue
+    card.append(card_number)
+    print('생성된 %d번째 카드 숫자=%d',k+1,card_number[k])  
+    print('\n')   
+
