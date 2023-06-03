851056f2026e29ec2a135a4b51018a1f2be8a05a
J0ngWon <129199902+J0ngWon@users.noreply.github.com>
2023-06-04 01:44:50 +0900

    Last

--- BlackJack.py
+++ BlackJack.py
@@ -1,46 +1,14 @@
-import random
-import math
+N, M = map(int, input().split())
+card = list(map(int, input().split()))
 
-'''def combination(n, r):
-    return math.factorial(n) // (math.factorial(r) * math.factorial(n - r))
-'''
-N=int(input())  #카드수 3~10
-M=int(input())  #최대수 10~300
-card=[]
+good_card = 0
+card_len = len(card)
 
-for k in range(N):
-    card_number=int(random.randint(1, M))
-    
-    card.append(card_number)
-    print('생성된 %d번째 카드 숫자=%d'%(k+1,card[k]))  
-    print('\n')   
+for i in range(card_len - 2):
+    for j in range(i + 1, card_len - 1):
+        for k in range(j + 1, card_len):
+            total = card[i] + card[j] + card[k]
+            if total <= M and total > good_card:
+                good_card = total
 
-
-
-'''for k in range(N):
-    if card[k]>=M :
-        del(card[k])'''
-
-count=int(0)
-result_card=[]
-card_len=len(card)     
-'''combination(card_len,3) #총 반복 횟수 조합이용 (nCr)'''
-
-good_card=int(card[0]+card[1]+card[2])
-q=2
-r=1
-
-for i in range(card_len-2):
-    for j in range(r,card_len):
-        if j-1==card_len:
-            r+=1
-        for k in range(q,card_len):
-            result_card.append(card[i]+card[j]+card[k])
-            if abs(M-result_card[count])<=abs(M-good_card):
-                good_card=result_card[count]
-           
-            if k-1==card_len:
-                q+=1
-            count+=1
-print('제출값=%d'%(good_card))   
-       
+print(good_card)
